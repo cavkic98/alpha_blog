db12685c7e29d920073d5a412a1fc11863bf7e0b
dr.Cavkic <dr.cavkic@gmail.com>
2018-04-08 21:31:14 +0200

    Kraj sekcije 3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,11 @@
 class ArticlesController < ApplicationController
 
+	def index
+
+		@articles = Article.all
+
+	end
+
 	def new
 		@article = Article.new
 	end
@@ -12,13 +18,39 @@ class ArticlesController < ApplicationController
 		else
 		render :new
 		end
+	end
 
+	def edit
+		@article = Article.find(params[:id])
+
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+		flash[:notice] = "Article was seccesfuly updated"
+		redirect_to article_path(@article )
+		else
+		render :edit
+		end
+
+		
 	end
 
 	def show
 		@article= Article.find(params[:id])
 
 	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Article was succesfully deleted"
+		redirect_to articles_path
+	end
+
+
 
 	private
 	def article_params
