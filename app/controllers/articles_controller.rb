2aca668a46fc828b9cac28b173c691bf5e882a3f
dr.Cavkic <dr.cavkic@gmail.com>
2018-04-11 16:08:26 +0200

    Dodane asocijacije i debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,9 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
+		
 		@article = Article.new(article_params)
+		@article.user = User.first
 		if @article.save
 		flash[:success] = "Article was seccesfuly created"
 		redirect_to article_path(@article )
