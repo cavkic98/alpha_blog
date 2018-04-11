cf8d4ea42ded985d79101b0b19e2d1fc04dc3231
dr.Cavkic <dr.cavkic@gmail.com>
2018-04-11 22:48:21 +0200

    Dodani useri i signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411133548) do
+ActiveRecord::Schema.define(version: 20180411143859) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180411133548) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
