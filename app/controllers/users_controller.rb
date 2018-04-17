0def372fdce4bfc16751aba31eab2da4650686bd
dr.Cavkic <dr.cavkic@gmail.com>
2018-04-18 01:06:09 +0200

    Dovrsene restrikcije

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-	before_action :set_user only: [:edit, :update, :show]
+	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
 
 	def index
 		
@@ -56,6 +57,16 @@ class UsersController < ApplicationController
 
 	def set_user
 		@user = User.find(params[:id])
+
+	end
+
+
+	def require_same_user
+
+		if current_user != @user &&
+			flash[:deleted] = "You can only edit and delite your own account"
+			redirect_to root_path
+		end
 
 	end
 
